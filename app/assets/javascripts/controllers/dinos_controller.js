9138621b81c98482b73378df8c935613be7f61e2
Steve Ellis <email@steveell.is>
2015-11-22 10:57:13 -0500

    carousel

--- app/assets/javascripts/controllers/dinos_controller.js
+++ app/assets/javascripts/controllers/dinos_controller.js
@@ -1,5 +1,27 @@
 RexUp.controller('dinosController', ['$scope', '$http', function ($scope, $http) {
   $http.get('dinos').success(function (data) {
     $scope.dinos = data.dinosaurs;
+    $scope.setDino(0);
   });
+
+  $scope.setDino = function setDino(index) {
+    $scope.dinoIndex = index;
+    $scope.dino = $scope.dinos[index];
+  }
+
+  $scope.nextDino = function nextDino() {
+    var newIndex = $scope.dinoIndex + 1;
+    if (newIndex > ($scope.dinos.length - 1)) {
+      newIndex = 0;
+    }
+    $scope.setDino(newIndex);
+  }
+
+  $scope.previousDino = function previousDino() {
+    var newIndex = $scope.dinoIndex - 1;
+    if (newIndex < 0) {
+      newIndex = $scope.dinos.length - 1;
+    }
+    $scope.setDino(newIndex);
+  }
 }]);
